e5220faf8abb14c666493b0899bb27ca5e444ce7
jdevcs <junaid@chainsafe.io>
2025-08-28 01:02:06 +0200

    conf path

--- tests/conftest.py
+++ tests/conftest.py
@@ -7,7 +7,7 @@ from reportlab.pdfgen import canvas
 from reportlab.lib.pagesizes import letter
 
 
-sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+#sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
 
 from app.main import app
