1a2855a547f0c8ec98ddfd70307ef5f4193a3bbc
jdevcs <junaid@chainsafe.io>
2025-08-28 02:10:49 +0200

    app name

--- setup.py
+++ setup.py
@@ -1,8 +1,9 @@
+# setup.py
 from setuptools import setup, find_packages
 
 setup(
-    name="app",
+    name="rag-platform-kit",
     version="0.1.0",
-    packages=find_packages(include=['app', 'app.*']),
+    packages=find_packages(include=["app*"]),
     package_dir={"": "."},
 )
