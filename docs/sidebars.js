4a2bd90baa2598aaab365f83cf28f2e44015aba2
jdevcs <junaid@chainsafe.io>
2025-10-11 20:16:51 +0200

    docs site

---
+++ docs/sidebars.js
@@ -0,0 +1,14 @@
+/**
+ * @type {import('@docusaurus/plugin-content-docs').SidebarsConfig}
+ */
+const sidebars = {
+  docsSidebar: [
+    'intro',
+    'overview',
+    'usage',
+    'production',
+    'ollama-setup',
+  ],
+};
+
+module.exports = sidebars;
